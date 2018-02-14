f3f888468a06575a3102b2729d9068fe3ac9f709
Doni Rubiagatra <rubiagatra@gmail.com>
2018-02-14 19:37:02 +0700

    prepare home page

--- newsapi/app.py
+++ newsapi/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from ner_api.home import HelloWorld
+from newsapi.home import HelloWorld
 from flask_jwt import JWT
-from ner_api.security import authenticate, identity
-from ner_api.db import create_user
+from newsapi.security import authenticate, identity
+from newsapi.db import create_user
 
 def create_app():
     
@@ -11,10 +11,10 @@ def create_app():
     app.config.from_object('config.settings')
     app.config.from_pyfile('settings.py', silent=True)
 
-    jwt = JWT(app, authenticate, identity)
+    JWT(app, authenticate, identity)
 
     @app.before_first_request
-    def initiate():
+    def initiate_mongodb():
         create_user()
 
 
